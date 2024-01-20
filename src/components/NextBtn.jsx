26eae4ee717b536d6c4b28a259082f66012c56a1
trung1007 <94527334+trung1007@users.noreply.github.com>
2024-01-20 11:56:21 +0700

    addSound

--- src/components/NextBtn.jsx
+++ src/components/NextBtn.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Colors, Btn, Font } from "../../GlobalStyles";
+// import Sound from "react-native-sound";
+import { Audio } from "expo-av";
+
 
 const NextBtn = ({ goNext }) => {
+  const [sound, setSound] = useState();
+
+  const onPressHandler=async()=>{
+    goNext(),
+    await playSound()
+  }
+  async function playSound(){
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(require('../../assets/sound/Btn_press1.mp3'));
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={goNext}>
+      <TouchableOpacity onPress={onPressHandler}>
         <Text style={styles.goNextText}>Tiếp tục</Text>
       </TouchableOpacity>
     </View>
---
+++ src/screens/TestScreen.jsx
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+var Sound = require('react-native-sound');
+
+
+Sound.setCategory('Playback');
+
+
+var ding = new Sound('ding.mp3', Sound.MAIN_BUNDLE, (error) => {
+if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+  // if loaded successfully
+  console.log('duration in seconds: ' + ding.getDuration() + 'number of channels: ' + ding.getNumberOfChannels());
+
+});
+const TestScreen = () => {
+  useEffect(() => {
+    ding.setVolume(1);
+    return () => {
+      ding.release();
+    };
+  }, []);
+  const playPause = () => {
+    ding.play(success => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.playBtn} onPress={playPause}>
+        <Ionicons name={'ios-play-outline'} size={36} color={'#fff'} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  playBtn: {
+    padding: 20,
+  },
+});
+export default TestScreen;
